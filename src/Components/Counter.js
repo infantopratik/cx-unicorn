0a4c3db4f3a8a43e100b72376a71d0abdae90d1d
User <user@Rentomojos-MacBook-Air.local>
2020-11-12 15:56:44 +0530

    Handle text change

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -5,7 +5,7 @@ export default class Counter extends Component {
     const { item, index, handleUpVote, handleDownVote } = this.props;
     return (
       <li>
-        <span>{item.name}</span> - <span>vote(s): {item.votes}</span>{" "}
+        <span>{item.name}</span> - <span>{item.votes > 0 ? "votes" : "vote"}: {item.votes}</span>{" "}
         <button onClick={() => handleUpVote(index)}>+</button>{" "}
         <button onClick={() => handleDownVote(index)}>-</button>
       </li>
